41d4fdefc771a51aad0e4f45f5e004c1124bf761
billowbashir <billowbashir@gmail.com>
2018-08-17 12:24:59 +0300

    generate username and repositories components ,generating a routing module and use the bootstrap cdn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsernameComponent } from './username/username.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsernameComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule
